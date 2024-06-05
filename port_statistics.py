8d1442eb52320d2ca1f0d75975dafe5f25e5adc0
epageler <evan_pageler@yahoo.com>
2024-06-05 12:02:01 -0500

    rename port_stats file, add stubs in port-stats

--- port_statistics.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jun  5 11:26:12 2024
-
-@author: evan_
-"""
-
---
+++ port_stats.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  5 11:26:12 2024
+
+@author: evan_
+"""
+import pandas as pd
+
+
+def get_daily_return(adj_close: pd.DataFrame) -> pd.DataFrame:
+    print("calculating daily_return df")
+
+
+def get_daily_ln_return(adj_close: pd.DataFrame) -> pd.DataFrame:
+    print("calculating daily_ln_return df")
+
+
+def get_correlation_matrix(adj_close: pd.DataFrame) -> pd.DataFrame:
+    print("calculating correlation_matrix df")
+
+
+def get_expected_returns(adj_close: pd.DataFrame) -> pd.DataFrame:
+    print("calculating expected_returns df")
+
+
+def get_std_deviations(adj_close: pd.DataFrame) -> pd.DataFrame:
+    print("calculating std_deviations df")
+
+
+def get_covariance_matrix(adj_close: pd.DataFrame) -> pd.DataFrame:
+    print("calculating covariance_matrix df")
+
+
+def get_inv_covariance_matrix(adj_close: pd.DataFrame) -> pd.DataFrame:
+    print("calculating inv_covariance_matrix df")
